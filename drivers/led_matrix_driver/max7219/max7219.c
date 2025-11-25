3616193c8e846c4ab9104565432fa1098ae4a2ad
Tche-Caver_adi <tche.caver@analog.com>
2025-11-24 17:56:52 -0800

    Added driver .h and .c files

---
+++ drivers/led_matrix_driver/max7219/max7219.c
@@ -0,0 +1,140 @@
+
+#define DT_DRV_COMPAT adi_max7219
+
+#include <errno.h>
+#include <zephyr/logging/log.h>
+
+#include "max7219.h"
+
+//Enable logging at CONFIG_LOG_DEFAULT_LEVEL
+LOG_MODULE_REGISTER(max7219);
+
+
+//Function declarations------------------------------------------------------
+
+static int max7219_reg_write(const struct device *dev, uint8_t reg, uint8_t data);
+static int max7219_init (const struct device *dev);
+static int led_matrix_test (const struct device *dev, uint8_t mode);
+static int led_matrix_write (const struct device *dev, uint8_t data);
+
+
+//Private functions (init function)------------------------------------------
+
+max7219_init(const struct device *dev){
+
+const struct max7219_config * cfg = (const struct max7219_config *)dev->config;
+const struct spi_dt_spec * led_drv = &cfg->bus;
+
+if (!spi_is_ready_dt(led_drv)){
+    LOG_ERR("Error initializing MAX7219\n");
+    return -1;
+}
+
+max7219_reg_write(dev, REG_SHUTDOWN, VAL_SHUTDOWN_ON);      //Shutdown register (0xXC) -> 0xX1 (enable)
+max7219_reg_write(dev, REG_DECODE, VAL_DECODE_NONE);        //Decode register (0xX9) -> 0x00 (no decode: each bit corresponds to an LED/segment)
+max7219_reg_write(dev, REG_INTENSITY, VAL_INTENSITY_MAX);   //Intensity register, duty cycle (0xXA) -> 0xXF (31/32 duty cycle, full intensity)
+max7219_reg_write(dev, REG_SCAN, VAL_SCAN_ALL_ON);           //Scan register (0xXB) -> 0xX7 (All digits on)
+
+printk("\nMAX7219 initialization complete.\n");
+
+}
+
+
+static int max7219_reg_write(const struct device *dev, uint8_t reg, uint8_t data){
+    
+    int ret = 0;
+
+    const struct max7219_config * cfg = (const struct max7219_config *)dev->config;
+    const struct spi_dt_spec * led_drv = &cfg->bus;
+
+    //Combine register and data into SPI packet for device
+    uint8_t packet[2] = {(reg & 0x0F), data};
+
+    //Zephyr SPI API structures
+    const struct spi_buf tx_buf = {.buf = packet, .len = sizeof(packet)};
+    const struct spi_buf_set tx = {.buffers = &tx_buf, .count = 1};
+    
+    //Perform SPI transaction, return value is ret
+    ret= spi_write_dt(&led_drv, &tx);
+
+    //Error handling
+    if (ret){
+
+        LOG_DBG("SPI transaction failed with code %d\n", ret);
+
+    };
+
+    return ret;
+
+}
+
+
+//Public functions (LED Matrix API)--------------------------------------------
+
+static int led_matrix_test (const struct device *dev, uint8_t mode){
+
+    const struct max7219_config * cfg = (const struct max7219_config *)dev->config;
+
+    if(mode){
+        max7219_reg_write(dev, REG_TEST, VAL_TEST_ENABLE);  //Test mode enabled, all LEDs on (0xXF) -> 0xX1  
+        printk("\nMAX7219 test mode enabled (all LEDs on).\n");
+    }
+    else{
+        max7219_reg_write(dev, REG_TEST, VAL_TEST_DISABLE);  //Test mode enabled, all LEDs off (0xXF) -> 0xX0
+        ("\nMAX7219 test mode disabled (all LEDs off).\n");
+    }
+}
+
+
+
+static int led_matrix_write (const struct device *dev, uint8_t *data){
+
+    const struct max7219_config * cfg = (const struct max7219_config *)dev->config;
+    
+    for(int i = 0; i < 8; i++){
+    max7219_reg_write(dev, (REG_LEDROW_BASE + i), (data + i));  
+    }
+
+    ("\nFinished LED matrix write.\n");
+}
+
+
+
+//Devicetree ------------------------------------------------------------------------
+
+
+//Define public functions of LED Matrix API
+
+//Assigning private led_matrix_test and led_matrix_write functions to public API (test, write)
+static const struct led_matrix_api led_matrix_api_functions = {
+    .test = led_matrix_test,
+    .write = led_matrix_write,
+};
+
+//MAX7219 instances
+#define MAX7219_DEFINE(inst)                \
+                                            \
+    //Config struct instances               \
+    //Zephyr will populate instance value, create config struct variable for each instance initialized in Devicetree
+    static const struct max7219_config max7219_config_##inst = {
+        .bus = SPI_DT_SPEC_INST_GET(inst,   \
+                    SPI_WORD_SET(8) |       \
+                    SPI_CS_ACTIVE_LOW |     \
+                    SPI_MODE_CPOL |         \
+                    SPI_TRANSFER_MSB,       \
+                    0),
+    }
+
+
+//Device instance created from devicetree node, register init function
+DEVICE_DT_INST_DEFINE(inst,                         \
+                      &max7219_init,                \
+                      NULL,                         \
+                      NULL,                         \
+                      &max7219_config_##inst,       \
+                      POST_KERNEL,                  \
+                      CONFIG_SENSOR_INIT_PRIORITY,  \
+                      &led_matrix_api_functions);   \
+
+
+
---
+++ drivers/led_matrix_driver/max7219/max7219.h
@@ -0,0 +1,32 @@
+#include <zephyr/drivers/spi.h>
+
+
+//Device registers
+#define REG_SHUTDOWN 0x0C
+#define REG_DECODE 0x09
+#define REG_INTENSITY 0x0A
+#define REG_SCAN 0x0B
+#define REG_TEST 0x0F
+#define REG_LEDROW_BASE 0x01
+
+//Initialization values
+#define VAL_SHUTDOWN_OFF 0x01
+#define VAL_SHUTDOWN_ON 0x01
+#define VAL_DECODE_NONE 0x00
+#define VAL_INTENSITY_MAX 0x0F
+#define VAL_SCAN_ALL_ON 0x07
+#define VAL_TEST_ENABLE  0x01
+#define VAL_TEST_DISABLE 0x00
+
+
+
+//Custom LED matrix driver API for MAX7219 with 8x8 LED matrix
+struct led_matrix_api{
+    int (*test)(const struct device *dev, uint8_t mode);     //Test mode that will turn all LEDs on/off depending on mode variable
+    int (*write)(const struct device *dev, uint8_t *data);    //8-element array of unsigned 8-bit int passed into data, bits of each element control each row of LEDs
+    
+}
+
+struct max7219_config {
+    struct spi_dt_spec bus;
+}
